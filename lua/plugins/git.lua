7390a1a23eb02aee6e1fd7a036e2155ec10b0640
Alex Marsh <amarsh@webstaurantstore.com>
2024-02-09 22:05:43 -0500

    add harpoon, add git keymaps, add c# lsp<CR>

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,11 +1,25 @@
 return {
     {
-        "tpope/vim-fugitive"
+        "tpope/vim-fugitive",
+        config = function()
+            vim.keymap.set('n', '<leader>gs', ':Git status<CR>')
+            vim.keymap.set('n', '<leader>gp', ':Git pull<CR>')
+            vim.keymap.set('n', '<leader>gP', ':Git push<CR>')
+            vim.keymap.set('n', '<leader>ga', ':Git add ' .. vim.fn.expand('%:p') .. '<CR>')
+            vim.keymap.set('n', '<leader>gA', ':Git add .<CR>')
+            vim.keymap.set('n', '<leader>gc', function()
+                local commit_message = vim.fn.input("Commit message: ")
+                vim.cmd(':Git commit -m "'.. commit_message .. '"<CR>')
+            end)
+        end
     },
     {
         "lewis6991/gitsigns.nvim",
         config = function()
-            require("gitsigns").setup()
+            local gitsigns = require('gitsigns')
+            gitsigns.setup()
+
+            vim.keymap.set('n', '<leader>ph', gitsigns.preview_hunk_inline)
         end
     }
 }
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -8,20 +8,16 @@ return {
         end,
     },
     {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls" }
-            })
-        end
-    },
-    {
         "neovim/nvim-lspconfig",
         dependencies = {
             'williamboman/mason.nvim',
+            "williamboman/mason-lspconfig.nvim",
             'folke/neodev.nvim'
         },
         config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = { "lua_ls", "omnisharp" }
+            })
             local lspconfig = require("lspconfig")
             local capabilities = require('cmp_nvim_lsp').default_capabilities()
             --local configs = require 'lspconfig.configs'
@@ -48,7 +44,11 @@ return {
                         }
                     }
                 },
-                capabilites = capabilities
+                capabilities = capabilities
+            })
+
+            lspconfig.omnisharp.setup({
+                capabilities = capabilities,
             })
 
             vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
