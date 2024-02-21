6ac9664f5176db62e19d9d2be87d34b163ee413b
Alex Marsh <amarsh@webstaurantstore.com>
2024-02-21 14:27:11 -0500

    Modify color scheme, shorten path lengths in telescope<CR>

--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -1,9 +1,19 @@
 return {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    lazy = false,
-    config = function()
-        vim.cmd.colorscheme "catppuccin-mocha"
-    end
+--    {
+--        "catppuccin/nvim",
+--        name = "catppuccin",
+--        priority = 1000,
+--        lazy = false,
+--        config = function()
+--            --        vim.cmd.colorscheme "catppuccin-mocha"
+--        end
+--    },
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            vim.cmd[[colorscheme tokyonight]]
+        end
+    }
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,6 +2,13 @@ return {
     'nvim-telescope/telescope.nvim', tag = '0.1.5',
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
+        require("telescope").setup({
+            defaults = {
+                path_display = {
+                    "truncate",
+                }
+            }
+        })
         local builtin = require("telescope.builtin")
         vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
         vim.keymap.set('n', '<leader>fs', builtin.live_grep, {})
