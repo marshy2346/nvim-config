7fbf7c8ed039fbddfaa960c7977f7bb7b8eca6c3
Alex Marsh <marshalex28@yahoo.com>
2024-05-10 13:55:58 -0400

    add treesitter context to config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,34 @@
 return {
-    'nvim-treesitter/nvim-treesitter',
-    build = ":TSUpdate",
-    config = function()
-        local configs = require("nvim-treesitter.configs")
+    {
+        'nvim-treesitter/nvim-treesitter',
+        build = ":TSUpdate",
+        config = function()
+            local configs = require("nvim-treesitter.configs")
 
-        configs.setup({
-            ensure_installed = {"c", "lua", "vim", "vimdoc", "query", "python", "javascript", "html", "css", "sql"},
-            sync_install = false,
-            highlight = {enable = true},
-            indent = {enable = true}
-        })
-    end
+            configs.setup({
+                ensure_installed = {"c", "lua", "vim", "vimdoc", "query", "python", "javascript", "html", "css", "sql"},
+                sync_install = false,
+                highlight = {enable = true},
+                indent = {enable = true}
+            })
+        end
+    },
+    {
+        'nvim-treesitter/nvim-treesitter-context',
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+            "tanvirtin/monokai.nvim"
+        },
+        config = function ()
+            require('treesitter-context').setup{
+                enable = true,
+                max_lines = 0,
+                line_numbers = true
+            }
+
+            local theme = require("monokai").classic
+
+            vim.cmd("hi TreeSitterContext guibg=" .. theme.base4)
+        end
+    }
 }
