f0ddadc22587f15438431442d7ef65d7ce3acfdc
Alex Marsh <marshalex28@yahoo.com>
2024-11-02 11:00:34 -0400

    Add dadbod, useful indent remap

--- lua/amarsh/remap.lua
+++ lua/amarsh/remap.lua
@@ -4,6 +4,9 @@ vim.keymap.set("n", "<leader>pv", "<CMD>Oil<CR>")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+vim.keymap.set("v", ">", ">gv")
+vim.keymap.set("v", "<", "<gv")
+
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
@@ -25,6 +28,7 @@ vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+vim.keymap.set("n", "<leader>D", [["_D]])
 
 -- This is going to get me cancelled
 vim.keymap.set("i", "<C-c>", "<Esc>")
--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -23,7 +23,7 @@ return {
         config = function()
             vim.cmd[[colorscheme monokai]]
             vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-            vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+            -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
         end
     }
 }
