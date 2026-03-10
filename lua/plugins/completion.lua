f7fb9f043d76430b78c0094bf0e11742b433a7fb
Alex Marsh <marshalex28@yahoo.com>
2026-03-10 16:31:55 -0400

    remove codeium

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,17 +1,5 @@
 return {
     {
-        "Exafunction/codeium.nvim",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "hrsh7th/nvim-cmp",
-        },
-        config = function()
-            require("codeium").setup({
-                enable_chat = true
-            })
-        end
-    },
-    {
         'hrsh7th/cmp-nvim-lsp'
     },
     {
@@ -52,7 +40,6 @@ return {
                     {
                         { name = 'nvim_lsp' },
                         { name = 'luasnip' }, -- For luasnip users.
-                        { name = 'codeium' }, -- For codeium users.
                     },
                     {
                         { name = 'buffer' },
